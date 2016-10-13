bb1f0ddef5160d9d284a2d94796011bbff2954ad
glejeune <gregoire.lejeune@free.fr>
2016-10-13 10:19:44 +0200

    Rename

--- src/rebar3_elixir.erl
+++ src/rebar3_elixir.erl
@@ -5,7 +5,7 @@
 init(State) ->
   lists:foldl(fun provider_init/2, {ok, State}, [rebar3_elixir_generate_mix
                                                  , rebar3_elixir_generate_lib
-                                                 , rebar3_elixir_generate_records]).
+                                                 , rebar3_elixir_generate_record]).
 
 provider_init(Module, {ok, State}) ->
   Module:init(State).
